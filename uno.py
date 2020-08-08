f6bbcb8d5fb33c716bfc8a547591a86f6f90715f
ayavuz2 <a.yavuzum@gmail.com>
2020-08-08 19:36:25 +0300

    Trying to make draw_card function more flexible. Needs a little more work on it.

--- uno.py
+++ uno.py
@@ -5,7 +5,7 @@ import random
 pygame.font.init()
 
 
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 775, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("UNO")
 
@@ -16,6 +16,7 @@ SPECIAL_CARD_TYPES = ['skip', 'reverse', '+2']
 COLOR_CARD_TYPES = NUMBERS + SPECIAL_CARD_TYPES*2
 BLACK_CARD_TYPES = ['wildcard', '+4']*4
 CARD_TYPES = NUMBERS + SPECIAL_CARD_TYPES + BLACK_CARD_TYPES
+CARD_WIDTH, CARD_HEIGHT = 72, 108
 
 # Creating a deck
 def create_deck(deck):
@@ -39,11 +40,11 @@ class Card():
 		self.png = pygame.image.load(os.path.join("images", "{}_{}.png".format(self.color, self.card_type)))
 
 	def __eq__(self, other):
-		return self.color == other.color and self.card_type == other.card_type
+		return self.color == other.color or self.card_type == other.card_type or self.color == 'black'
 
-	def draw_card(self, win, width, height, gap):		
-		win.blit(self.png, (gap, height-150))
-
+	def draw_card(self, win, x, y, gap):		
+		win.blit(self.png, (x%WIDTH, y-(CARD_HEIGHT+gap))) # update it with flexible heights
+		# win.blit(self.png, (x%WIDTH, y-(CARD_HEIGHT+gap)))
 
 class Player():
 	def __init__(self, name):
@@ -52,12 +53,11 @@ class Player():
 
 	def draw_a_card_from_deck(self, deck):
 		colors = list(deck.keys())
-		weights = []
 
+		# Adding probability to the colors depending on how many cards they do have
+		weights = []
 		for clr in colors:
 			weights.append(len(deck[clr]))
-
-		print(weights)
 		color = random.choices(colors, weights=weights, k=1) # k is the number of choosing
 		color = ''.join(color) # list to string
 
@@ -65,8 +65,11 @@ class Player():
 		self.player_cards.append(card)
 
 	def set_initial_cards(self, deck):
-		for i in range(7):
+		for i in range(9):
 			self.draw_a_card_from_deck(deck) 
+
+	def get_len(self):
+		return len(self.player_cards)
 
 	def is_finished(self):
 		return len(self.player_cards) == 0
@@ -74,43 +77,51 @@ class Player():
 	def is_playable(self, card_index, middle_card):
 		pass
 
-	def draw_players_cards(self, win, width, height):
-		gap = 10
+	def draw_players_cards(self, win, width, height, gap):		
+		total_gap = gap
 		for card in self.player_cards: # try [:] 
-			card.draw_card(win, width+0, height, gap)
-			gap += 80
+			card.draw_card(win, total_gap, height, gap) # make (x, y) flexible for draw_card function
+			total_gap += (CARD_WIDTH + gap)
 
 
-def get_click_pos(pos): # work in progress...
-	print(pos)
+def get_click_pos(pos, width, height, gap, number_of_cards): # work in progress...
+	x, y = pos
+	number_of_rows = (number_of_cards*CARD_WIDTH + (number_of_cards+1)*gap)//width + 1
 
-def draw(win, width, height, player):
+	# checking if the cursor is in a specific area and also not on the gaps
+	if y>height-(number_of_rows*CARD_HEIGHT + (number_of_rows+1)*gap) and (height-y) % (CARD_HEIGHT+gap) >= gap:
+		row = (height-y)//(CARD_HEIGHT+gap)
+		if x % (CARD_WIDTH+gap) >= gap:
+			col = x//CARD_WIDTH
+			return row, col
+
+	return None, None
+
+def draw(win, width, height, player, gap):
 	win.fill((255,255,255))
 
-	player.draw_players_cards(win, width, height)
+	player.draw_players_cards(win, width, height, gap)
 
 	pygame.display.update()
 
 
-def main(win):
+def main(win, width, height):
 	run = True
-	FPS = 15
+	FPS = 30
+	clock = pygame.time.Clock()
 
 	DECK = dict()
-	clock = pygame.time.Clock()
+	MID_CARD = []
+	GAP = 5
 
 	create_deck(DECK)
 
 	player = Player('Tom')
 	player.set_initial_cards(DECK)
-	
-
-	# print("*********************")
-	# print(DECK)
 
 	while run:
 		clock.tick(FPS)
-		draw(win, WIDTH, HEIGHT, player)
+		draw(win, width, height, player, GAP)
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -118,7 +129,12 @@ def main(win):
 
 		if pygame.mouse.get_pressed()[0]:
 			pos = pygame.mouse.get_pos()
-			get_click_pos(pos)
+			# print(pos)
+			row, col = get_click_pos(pos, width, height, GAP, player.get_len())
+			print(row, col)
+
+		if pygame.mouse.get_pressed()[2]: # Changing card positions
+			pass 
 
 
-main(WIN)
+main(WIN, WIDTH, HEIGHT)
